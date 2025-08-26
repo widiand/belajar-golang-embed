5df97485a10f1d80cded342d3b678b651a6e1154
widiand <widiand36@gmail.com>
2025-08-26 11:27:04 +0700

    embed file to []byte
    for binary file like image

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package belajargolangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -15,4 +17,14 @@ var version2 string
 func TestString(t *testing.T) {
 	fmt.Println(version)
 	fmt.Println(version2)
+}
+
+//go:embed cat.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("cat_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
