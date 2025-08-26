1e4ec9baccdc093eb0d4bac1aabdc5a2b4306456
widiand <widiand36@gmail.com>
2025-08-26 11:19:10 +0700

    embed file to string

---
+++ embed_test.go
@@ -0,0 +1,18 @@
+package belajargolangembed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed version.txt
+var version2 string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+	fmt.Println(version2)
+}
